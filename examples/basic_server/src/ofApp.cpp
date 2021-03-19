7f6b103a2033633bf4fe210fdc4d20cdb469f5b9
Cris <c@matadata.com>
2021-03-19 17:01:44 -0400

    Small changes to server example

--- examples/basic_server/src/ofApp.cpp
+++ examples/basic_server/src/ofApp.cpp
@@ -5,7 +5,7 @@ void ofApp::setup()
 {
 	ofSetLogLevel(OF_LOG_VERBOSE);
 	// Always give the ofParameterGroup a name:
-	mainGroup.setName("Parameter Server Simple Example");
+	mainGroup.setName("ofxRemoteParameters Simple Example");
 	// Add the parameters to the group:
 	mainGroup.add(circleRadius.set("Circle Radius", 10, 1, 500),
 				  circleColor.set("Circle Color", ofColor::orange),
@@ -25,16 +25,15 @@ void ofApp::setup()
 	ofSetCircleResolution(40);
 
 	// Note that if you use the GUI the client does not update automatically. If you want the client to update
-	// you will need to call paramServer.syncParameters() whenever a parameter does change. Check the advanced example
-	// for more info.
+	// you will need to call paramServer.syncParameters() whenever a parameter does change.
 }
 
 void ofApp::draw()
 {
 	ofBackgroundGradient(ofColor::gray, ofColor::blueSteel);
-	gui.draw();
 	ofFill();
 	ofSetColor(circleColor);
 	ofDrawEllipse(circlePosition, circleRadius * 0.5, circleRadius * 0.5);
+	gui.draw();
 }
 
